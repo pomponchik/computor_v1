2b334be8dc1d117ea79d37ffd8e9f6e7054a671c
pomponchik <zheni-b@yandex.ru>
2020-10-14 21:35:44 +0300

    normalization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sys import argv
 from srcs.basic_prover import BasicProver
 from srcs.tokenizator import Tokenizator
+from srcs.normalizator import Normalizator
 from srcs.groups_maker import GroupsMaker
 
 
@@ -9,6 +10,7 @@ if __name__ == '__main__':
         print('Incorrect input. Usage: ./computor "{your expression}"')
         exit(1)
     source_string = argv[1]
+    source_string = Normalizator(source_string).get_norm_version()
     BasicProver(source_string).go()
     tokens_list = Tokenizator(source_string).get_list()
     expression = GroupsMaker(tokens_list).get_pieces()
