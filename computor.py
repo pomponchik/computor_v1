771bcd43a81da75cd79cde44f9957352ff9b47d6
pomponchik <zheni-b@yandex.ru>
2020-11-26 15:47:32 +0300

    usage

--- computor.py
+++ computor.py
@@ -7,7 +7,7 @@ from srcs.groups_maker import GroupsMaker
 
 if __name__ == '__main__':
     if len(argv) != 2:
-        print('Incorrect input. Usage: ./computor "{your expression}"')
+        print('Incorrect input. Usage: python3 computor.py "{your expression}"')
         exit(1)
     source_string = argv[1]
     source_string = Normalizator(source_string).get_norm_version()
